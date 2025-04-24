a87ac205208b61570e90e354e8f767a2cbabc91b
Devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-04-24 23:40:25 +0530

    Easy problem of 150

---
+++ 169. Majority Element.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        Integer maj = null;
+        
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                maj = nums[i];
+            } else if (nums[i] == maj) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        // Since it is guaranteed that the majority element exists, so we don't check for the frequency of "maj" and return
+        return maj;
+    }
+}
